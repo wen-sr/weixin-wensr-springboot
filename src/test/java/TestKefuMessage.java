321441659f49ad1edd80c9700f181fd54099dc9d
wen-sr <793818714@qq.com>
2017-09-12 20:35:51 +0800

    a

--- src/test/java/TestKefuMessage.java
+++ src/test/java/TestKefuMessage.java
@@ -1,31 +1,33 @@
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.exception.WxErrorException;
-import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
 import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Description:
  * User: wen-sr
  * Date: 2017-09-12  15:49
  */
+@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TestKefuMessage {
 
+    @Autowired
+    private WxMpService wxMpService;
 
     @Test
     public  void testkefuMessage(){
-        WxMpInMemoryConfigStorage wxMpInMemoryConfigStorage = new WxMpInMemoryConfigStorage();
-        wxMpInMemoryConfigStorage.setAppId("wxa77d1ad158f5eef7");
-        wxMpInMemoryConfigStorage.setSecret("3fadea5fae3f2aa98fe146ad9ed5d99e");
-        wxMpInMemoryConfigStorage.setToken("jxxhwl");
+        //WxMpInMemoryConfigStorage wxMpInMemoryConfigStorage = new WxMpInMemoryConfigStorage();
+        //wxMpInMemoryConfigStorage.setAppId("wxa77d1ad158f5eef7");
+        //wxMpInMemoryConfigStorage.setSecret("3fadea5fae3f2aa98fe146ad9ed5d99e");
+        //wxMpInMemoryConfigStorage.setToken("jxxhwl");
 
-        WxMpService wxMpService = new WxMpServiceImpl();
-        wxMpService.setWxMpConfigStorage(wxMpInMemoryConfigStorage);
         WxMpKefuMessage message = new WxMpKefuMessage();
         message.setContent("这是客服消息!!!!!!!!!");
         message.setMsgType(WxConsts.XML_MSG_TEXT);
